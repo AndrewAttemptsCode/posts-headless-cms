51aa2751f2f99752c87142570cb478152661c257
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-08 20:15:29 +0000

    feat: add logo component to header section

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,17 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Logo from "./Logo";
 
 const RootLayout = () => {
   return (
-    <div>
+    <div className="grid h-dvh grid-rows-[auto_1fr_auto] bg-gray-100 dark:bg-gray-700">
       <header>
-        header content
+        <Logo />
       </header>
       <main>
         <Outlet />
       </main>
-      <footer>
-        footer content
-      </footer>
+      <footer>footer content</footer>
     </div>
   );
 };
