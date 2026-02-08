14c63fb44d68dfc7c16f7df500f405328dab34fa
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-08 18:09:03 +0000

    feat: add child routes for root layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,26 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
+import PostsPage from "./pages/PostsPage";
+import PostPage from "./pages/PostPage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to={"/posts"} replace />
+      },
+      {
+        path: "/posts",
+        element: <PostsPage />
+      },
+      {
+        path: "/posts/:slug",
+        element: <PostPage />
+      }
+    ],
   },
 ]);
 
