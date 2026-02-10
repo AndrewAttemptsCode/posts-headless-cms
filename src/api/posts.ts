b20080102b275635bdaaee8479bd49415c114e46
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-10 18:52:09 +0000

    fix: update fetch url to include access_token

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -3,7 +3,7 @@ const space = import.meta.env.VITE_SPACE;
 const accesToken = import.meta.env.VITE_ACCESS_TOKEN;
 
 export const fetchPosts = async () => {
-  const response = await fetch(`${baseURL}/spaces/${space}/entries?${accesToken}&content_type=blogPost`);
+  const response = await fetch(`${baseURL}/spaces/${space}/entries?access_token=${accesToken}&content_type=blogPost`);
   
   if (!response.ok) throw new Error("Failed to fetch posts");
 
@@ -11,7 +11,7 @@ export const fetchPosts = async () => {
 }
 
 export const fetchPost = async (slug: string) => {
-  const response = await fetch(`${baseURL}/spaces/${space}/entries?${accesToken}&content_type=blogPost&fields.slug=${slug}`);
+  const response = await fetch(`${baseURL}/spaces/${space}/entries?access_token=${accesToken}&content_type=blogPost&fields.slug=${slug}`);
 
   if (!response.ok) throw new Error("Failed to fetch post");
 
