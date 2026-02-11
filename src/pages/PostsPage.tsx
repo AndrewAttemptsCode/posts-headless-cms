f74637430f2124154986e4d51deec2ee23aec308
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-11 21:46:29 +0000

    chore: run prettier formatter

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,12 +1,11 @@
 import PostsList from "../components/PostsList";
 
 const PostsPage = () => {
-
   return (
     <div>
       <PostsList />
     </div>
-  );  
+  );
 };
 
 export default PostsPage;
