07aaec22f82ecb746a74038793142ae687faf6d8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-11 21:46:05 +0000

    feat: add PostList to map over each post and display as a list

---
+++ src/components/PostsList.tsx
@@ -0,0 +1,23 @@
+import usePosts from "../hooks/usePosts";
+import PostCard from "./PostCard";
+
+const PostsList = () => {
+  const { posts, loading, error } = usePosts();
+
+  return (
+    <div className="flex flex-col p-4">
+      <section className="w-full max-w-7xl self-center">
+        <h2 className="mb-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+          Latest Posts
+        </h2>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
+          {posts.map((post) => (
+            <PostCard key={post.slug} post={post} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PostsList;
