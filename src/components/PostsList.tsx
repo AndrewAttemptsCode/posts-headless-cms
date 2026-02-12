3bc13f42149fcaefae1a1da02053d944bbd61e72
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-12 18:41:24 +0000

    feat: add LoadingSpinner to render if loading is true, else display posts list cards

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,4 +1,5 @@
 import usePosts from "../hooks/usePosts";
+import LoadingSpinner from "./LoadingSpinner";
 import PostCard from "./PostCard";
 
 const PostsList = () => {
@@ -10,11 +11,15 @@ const PostsList = () => {
         <h2 className="mb-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">
           Latest Posts
         </h2>
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
         <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4">
           {posts.map((post) => (
             <PostCard key={post.slug} post={post} />
           ))}
         </div>
+        )}
       </section>
     </div>
   );
