293d57fe15fc882ce10d8026d29ad01d41177278
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-12 19:46:25 +0000

    feat: add link to the logo to navigate back to home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => {
   return (
     <div className="py-4">
       <div className="relative justify-self-center p-2">
-        <h1 className="relative z-5 text-3xl font-semibold text-gray-200 dark:text-gray-700">
-          Posty
-        </h1>
-        <div className="animate-logoDrop absolute inset-0 origin-top-left bg-gray-500 dark:bg-gray-300"></div>
-        <div className="absolute inset-0 bg-gray-700 dark:bg-gray-200"></div>
+        <Link to={"/"}>
+          <h1 className="relative z-5 text-3xl font-semibold text-gray-200 dark:text-gray-700">
+            Posty
+          </h1>
+          <div className="animate-logoDrop absolute inset-0 origin-top-left bg-gray-500 dark:bg-gray-300"></div>
+          <div className="absolute inset-0 bg-gray-700 dark:bg-gray-200"></div>
+        </Link>
       </div>
     </div>
   );
