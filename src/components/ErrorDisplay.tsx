075b971b920c33613bc092e84e83ef27dac984f5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-12 19:15:13 +0000

    feat: add error display

---
+++ src/components/ErrorDisplay.tsx
@@ -0,0 +1,13 @@
+type ErrorProps = {
+  error: string;
+};
+
+const ErrorDisplay = ({ error }: ErrorProps) => {
+  return (
+    <div className="w-fit rounded-lg border-2 border-red-400/80 bg-red-300 p-2">
+      <p className="font-semibold text-balance text-gray-700">{error}</p>
+    </div>
+  );
+};
+
+export default ErrorDisplay;
