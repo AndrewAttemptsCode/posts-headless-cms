023a12f464f6277fc49fa5ef93426a83f8398e92
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-12 19:39:39 +0000

    feat: add credits to footer

---
+++ src/components/FooterCredits.tsx
@@ -0,0 +1,11 @@
+const FooterCredits = () => {
+  return (
+    <div className="py-4">
+      <div className="justify-self-center font-semibold text-gray-700 dark:text-gray-200">
+        <p>&copy; Andrew Travis {new Date().getFullYear()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FooterCredits;
