bbc93ba8861489139ac328e4da3b6d185919fd9a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-14 21:36:16 +0000

    style: update container to display in the center

--- src/components/ErrorDisplay.tsx
+++ src/components/ErrorDisplay.tsx
@@ -4,7 +4,7 @@ type ErrorProps = {
 
 const ErrorDisplay = ({ error }: ErrorProps) => {
   return (
-    <div className="w-fit rounded-lg border-2 border-red-400/80 bg-red-300 p-2">
+    <div className="w-fit rounded-lg border-2 border-red-400/80 bg-red-300 p-2 justify-self-center">
       <p className="font-semibold text-balance text-gray-700">{error}</p>
     </div>
   );
