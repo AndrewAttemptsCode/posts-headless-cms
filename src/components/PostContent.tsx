d5516f22cd0c188c14e86072f8c0d32e14594051
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-02-14 18:27:49 +0000

    style: add content structure and styling

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -14,27 +14,27 @@ const PostContent = () => {
   const post = location.state?.post;
 
   return (
-    <div className="w-[90%] max-w-3xl justify-self-center py-4">
-      <h2 className="text-2xl font-semibold text-gray-700 dark:text-gray-200">
-        {post.title}
-      </h2>
-      <p>
-        {post.author.firstName} {post.author.lastName}
-      </p>
-      <p>{new Date(post.date).toLocaleString()}</p>
-      <div>
+    <article className="w-[90%] max-w-3xl justify-self-center py-4 text-gray-700 dark:text-gray-200">
+      <h2 className="text-2xl font-semibold">{post.title}</h2>
+      <div className="mb-2 bg-gray-200 p-1 sm:p-2 dark:bg-gray-600">
+        <p>
+          {post.author.firstName} {post.author.lastName}
+        </p>
+        <p>
+          <strong>Published:</strong> {new Date(post.date).toLocaleString()}
+        </p>
+      </div>
+      <div className="space-y-4">
         <img
           src={post.image || undefined}
           alt=""
-          className="mb-2 aspect-3/2 object-cover md:float-right md:mb-0.5 md:ml-1 md:w-1/2"
+          className="aspect-video object-cover sm:aspect-21/9"
         />
         {post.body.map((paragraph, index) => (
-          <p key={index} className="mb-4">
-            {paragraph}
-          </p>
+          <p key={index}>{paragraph}</p>
         ))}
       </div>
-    </div>
+    </article>
   );
 };
 
